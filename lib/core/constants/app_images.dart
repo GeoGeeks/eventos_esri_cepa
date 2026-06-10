757456751f55a62887efe7085f7b436b7c439104
Diego <garcia302nacional@gmail.com>
2026-06-10 14:12:07 -0500

    refactor: create app images constants and organize assets folders

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,23 @@
+class AppImages {
+
+  static const onboarding1 =
+      'assets/images/onboarding/onboarding_1.png';
+
+  static const onboarding2 =
+      'assets/images/onboarding/onboarding_2.png';
+
+  static const onboarding3 =
+      'assets/images/onboarding/onboarding_3.png';
+
+  static const onboarding4 =
+      'assets/images/onboarding/onboarding_4.png';
+
+  static const profileLogo =
+      'assets/images/profile/profile_1.png';
+
+  static const profileHeader =
+      'assets/images/profile/profile_2.png';
+
+  static const inicioHeader =
+      'assets/images/inicio/header_inicio.png';
+}
