47989146da2427f14436e8a4ce88244ab738431c
diego659 <garcia302nacional@gmail.com>
2026-07-09 09:25:56 -0500

    Fix font configuration and update pubspec

--- lib/core/constants/fonts.dart
+++ lib/core/constants/fonts.dart
@@ -5,7 +5,6 @@ class Fonts {
   static const String bold       = 'AvenirNextBold';
   static const String demi       = 'AvenirNextDemi';
   static const String medium     = 'AvenirNextMedium';
-  static const String heavy      = 'AvenirNextHeavy';
   static const String light      = 'AvenirNextLight';
   static const String thin       = 'AvenirNextThin';
   static const String ultraLight = 'AvenirNextUltraLight';
